7c8f23605fb14cb7a17585885de0220079ac8897
CallMeAlb <albert.langepas@gmail.com>
2026-08-07 18:08:50 +0500

    add portfolio

---
+++ data/portfolio.py
@@ -0,0 +1,17 @@
+PORTFOLIO = [
+    {
+        "title": "Свадебная съемка",
+        "description": "Полный свадебный день",
+        "image": "assets/portfolio/work1.jpg",
+    },
+    {
+        "title": "Рекламный ролик",
+        "description": "Видео для бизнеса",
+        "image": "assets/portfolio/work2.jpg",
+    },
+    {
+        "title": "Контент для Instagram",
+        "description": "Серия коротких вертикальных роликов",
+        "image": "assets/portfolio/work3.jpg",
+    },
+]
